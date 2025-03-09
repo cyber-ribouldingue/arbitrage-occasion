a60e4253daffece26c5d03a8b0442da488d988cc
cyber-ribouldingue <ribouldingue@tutanota.com>
2025-03-09 16:49:18 +0100

    Create config.py
    Ajout de config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# URLs des sites à scraper
+LEBONCOIN_URL = "https://www.leboncoin.fr/recherche?category=smartphones"
+EBAY_URL = "https://www.ebay.fr/sch/i.html?_nkw=occasion"
+AMAZON_URL = "https://www.amazon.fr/s?i=merchant-items&rh=p_n_condition-type:2338192011"
+
+# Paramètres d'analyse
+MIN_PROFIT_RATIO = 15
+REPORT_FOLDER = "docs/assets/reports"
+
+# Fichier pour sauvegarder le modèle IA
+IA_MODEL_FILE = "model.pkl"
